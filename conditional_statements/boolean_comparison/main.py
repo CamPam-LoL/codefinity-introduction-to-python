a0df2155e00f3318eea2a986b220286f206cb7ff
CamPam-LoL <>
2026-02-01 22:56:48 +0000

    Boolean Comparison

---
+++ conditional_statements/boolean_comparison/main.py
@@ -0,0 +1,11 @@
+#variable represeting total cost instatiation
+total_cost = 25.00
+
+#Discount variable
+discountEligible = total_cost >= 20.00
+
+#Test Print function
+#print(discountEligible)
+
+#Final Print Messaege
+print(f"Is the purchase eligible for a discount? {discountEligible}")
