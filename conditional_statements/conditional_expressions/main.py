baa7e5ee9c1144ac9d622e9fe5a8d39b6b4c0e62
CamPam-LoL <>
2026-02-03 01:00:40 +0000

    Conditional expressions

---
+++ conditional_statements/conditional_expressions/main.py
@@ -0,0 +1,16 @@
+# Input variables
+product_type = "Vegetables"
+day_of_week = "Monday"
+
+#if fruit and monday , Fruit discount
+if product_type = "Fruits" and day_of_week = "Monday":
+    print("Fruit discount!")
+
+#if Vegetable and monday , Vegable Discount
+elif product_type = "Vegetables" and day_of_week == "Monday":
+    print("Vegatables discount!")
+
+#Neither are true , No Discount
+else:
+    print("No discount")
+
