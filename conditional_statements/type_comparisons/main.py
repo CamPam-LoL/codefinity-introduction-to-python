d63b746dce89cd077c4dd992e83f9ec4203c498c
CamPam-LoL <>
2026-02-03 00:42:34 +0000

    Type Comparisons

---
+++ conditional_statements/type_comparisons/main.py
@@ -0,0 +1,16 @@
+# Product details
+description = "Imported honey, raw and unfiltered"
+price = "5.99"
+count = 120
+
+# Write your code here
+
+#If price is float 
+price_is_float = type(price) == float
+
+#If count is inteer
+count_is_int = type(count) == int
+
+
+print("Is price a float?:", price_is_float)
+print("Is count an integer?:", count_is_int)
